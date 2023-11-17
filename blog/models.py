4da2e65457f3175803f87f329e10686a21fece50
Mereena <mereenajoy1999@gmail.com>
2023-11-17 19:16:58 +0530

    model added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class PostModel(models.Model):
+    userid = models.CharField(max_length=100 , default="")
+    title = models.CharField(max_length=100 , default="")
+    message = models.CharField(max_length=500 , default="")
+
+
+class UserModel(models.Model):
+    name =models.CharField(max_length=100 , default="")
+    profile =models.CharField(max_length=100 , default="")
+    email=models.CharField(max_length=100 , default="")
+    password =models.CharField(max_length=100 , default="")
